63c1044d56e92110c221b0ee04e729dde1c71082
sacom1441 <samir.comas1@gmail.com>
2023-07-21 11:13:12 +0200

    sprint5 nivell1 exercici3 v.1

--- index.js
+++ index.js
@@ -15,15 +15,34 @@ const options = {
         'Accept': 'application/json'
     }
 };
+const reportAcudits = []; //guardamos array de objetos.
+let selectedScore = 0; //guardamos el número seleccionado de los botones antes de enviarlo.
 const processTheJoke = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const response = yield fetch(url, options);
-        const message = yield response.json();
+        const message = yield response.json(); //lo pasamos a json
         console.log(message.joke);
         let joke = document.getElementById('jokeMessage');
-        joke.innerHTML = `" ${message.joke} "`;
+        joke.innerHTML = `" ${message.joke} "`; //imprimimos el mensaje por pantalla
+        const jokeScore = {
+            joke: message.joke,
+            score: selectedScore,
+            date: selectedScore !== 0 ? new Date().toDateString() : 0,
+        };
+        console.log(`joke: ${jokeScore.joke}, score: ${jokeScore.score}, date: ${jokeScore.date} `);
+        reportAcudits.push(jokeScore);
+        selectedScore = 0; // es para reiniciar el valaor de la puntuación seleccionada.
     }
     catch (error) {
         console.error(error);
     }
 });
+function valorateScore() {
+    console.log(reportAcudits[reportAcudits.length - 1]);
+}
+const score = (score) => {
+    if (reportAcudits.length > 0) {
+        selectedScore = selectedScore === score ? 0 : score;
+        console.log(`score seleccionada: ${selectedScore}`);
+    }
+};
--- index.ts
+++ index.ts
@@ -5,17 +5,42 @@ const options = {
         'Accept': 'application/json'
     }
 };
+interface Joke {
+    joke: string;
+    score: number | 0;
+    date: string | 0;
+}
+const reportAcudits: Joke[] = []; //guardamos array de objetos.
+let selectedScore: number | 0 = 0; //guardamos el número seleccionado de los botones antes de enviarlo.
 
 const processTheJoke = async () => {
     try {
         const response = await fetch(url, options)
-        const message = await response.json();
+        const message = await response.json(); //lo pasamos a json
         console.log(message.joke);
         let joke = document.getElementById('jokeMessage') as HTMLParagraphElement;
-        joke.innerHTML = `" ${message.joke} "`;
-        
+        joke.innerHTML = `" ${message.joke} "`; //imprimimos el mensaje por pantalla
+
+        const jokeScore: Joke = {  //crear objeto de los chistes
+            joke: message.joke,
+            score: selectedScore,
+            date: selectedScore !== 0 ? new Date().toDateString() : 0,
+        };
+        console.log(`joke: ${jokeScore.joke}, score: ${jokeScore.score}, date: ${jokeScore.date} `);
+        reportAcudits.push(jokeScore);
+        selectedScore = 0; // es para reiniciar el valaor de la puntuación seleccionada.
+
     } catch (error) {
         console.error(error);
     }
-
 }
+function valorateScore(){
+    console.log(reportAcudits[reportAcudits.length-1]);
+}
+const score = (score: number) => {
+    if (reportAcudits.length > 0) {
+        selectedScore = selectedScore === score ? 0 : score;
+        console.log(`score seleccionada: ${selectedScore}`);
+    }
+}
+
